87ae3e52b29d915bfaf1961c30f4cf3912e64b22
KRochaS <karinerocha.s@hotmail.com>
2026-02-03 22:20:50 -0300

    chore: Troca page router pra app router

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import '@/styles/globals.css';
+import type { Metadata } from 'next';
+import { Work_Sans, Satisfy } from 'next/font/google';
+import { Header } from '@/presentation/shared/components/header/Header';
+
+const workSansDisplay = Work_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  display: 'swap',
+});
+
+const satisfyDisplay = Satisfy({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-satisfy',
+  display: 'swap',
+});
+
+export const metadata: Metadata = {
+  title: 'Concerts Recap',
+  description: 'Log and organize your concert memories',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="app-background">
+      <body
+        className={`${workSansDisplay.className} ${satisfyDisplay.variable} antialiased text-content-body h-screen`}
+      >
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
--- src/app/new-concert/page.tsx
+++ src/app/new-concert/page.tsx
@@ -1,6 +1,8 @@
-import { useNewConcertFlow } from "@/presentation/hooks/newConcert/useNewConcertFlow";
-import { Layout } from "@/presentation/pages/newConcert/layout/Layout";
-import { STEP_COMPONENT } from "@/presentation/ui-model/newConcert/newConcertFlow";
+'use client';
+
+import { useNewConcertFlow } from '@/presentation/hooks/newConcert/useNewConcertFlow';
+import { Layout } from '@/presentation/pages/newConcert/layout/Layout';
+import { STEP_COMPONENT } from '@/presentation/ui-model/newConcert/newConcertFlow';
 
 export default function NewConcertPage() {
   const { step, goToInitial, goToOrganize } = useNewConcertFlow();
