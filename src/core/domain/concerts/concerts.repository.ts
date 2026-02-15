5c1140000fcff86a6f7d276f1606ff3e58eacab4
KRochaS <karinerocha.s@hotmail.com>
2026-02-15 20:57:27 -0300

    feat: remove searchConcertUseCase

--- src/core/domain/concerts/concerts.repository.ts
+++ src/core/domain/concerts/concerts.repository.ts
@@ -1,8 +1,6 @@
-import { Concert, ConcertSummary } from './concert.entity';
+import { ConcertSummary } from './concert.entity';
 
 export interface ConcertRepository {
-  findMany(): Promise<Concert[]>;
-  searchMany(term: string): Promise<Concert[]>;
   findManySummaries(): Promise<ConcertSummary[]>;
   searchManySummaries(term: string): Promise<ConcertSummary[]>;
 }
