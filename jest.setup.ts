2e247602cbd29901f48c4133e38d306fe964295a
KRochaS <karinerocha.s@hotmail.com>
2026-02-16 15:27:32 -0300

    chore: add browser API polyfills to jest setup and remove unnecessary mock

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import '@testing-library/jest-dom';
+
+import { webcrypto } from 'crypto';
+import { TextDecoder, TextEncoder } from 'util';
+globalThis.TextEncoder = TextEncoder;
+globalThis.TextDecoder = TextDecoder;
+if (!globalThis.crypto) {
+  (globalThis as any).crypto = webcrypto;
+}
 
 expect.extend({});
